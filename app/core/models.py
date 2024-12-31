95c17a1ce05319bfe5329d04d6e940864f67ea2d
H Rawat <hoshiyar.rawat@gmail.com>
2024-12-31 06:53:33 +0530

    update code

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,67 @@
-from django.db import models # noqa
+"""
+Database models.
+"""
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+class UserManager(BaseUserManager):
+    """Manager for Users."""
+
+    def create_user(self, email, password=None, **extra_fileds):
+
+        """Create save and return a new user"""
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fileds)
+        print(f'Test add: user name***************************** = {user}')
+        user.set_password(password)
+        print(f'Test add: user Creat = {user}')
+        user.save(using=self._db)
+        print(f'Test add: user Created = {user}')
+        return user
+
+    def create_superuser(self, email, password):
+        """Create super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+class Recipe(models.Model):
+    """Recipe Model"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
